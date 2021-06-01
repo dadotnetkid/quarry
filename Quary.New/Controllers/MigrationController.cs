d8a6aa868467e0bc9f03d85ba651d9a655209907
Mark Christopher Cacal <markchristopher.cacal@gmail.com>
2021-06-01 08:33:18 +0800

    add migration end point

---
+++ Quary.New/Controllers/MigrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FluentMigrator.Infrastructure;
+using FluentMigrator.Runner;
+
+namespace Quary.New.Controllers
+{
+    public class MigrationController : Controller
+    {
+        private readonly IMigrationRunner _migrationRunner;
+
+        // GET: Migration
+        public MigrationController()
+        {
+            _migrationRunner = DependencyResolver.Current.GetService<IMigrationRunner>();
+        }
+
+        public ActionResult Index()
+        {
+            try
+            {
+                _migrationRunner.MigrateUp();
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+            }
+            return View();
+        }
+    }
+}
